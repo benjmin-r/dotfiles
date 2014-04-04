522a1ac45a31f97c442b3976ed845e306d134b74
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-04-04 13:51:38 +0200

    using homebrews python site-package path

--- shell/env.bash
+++ shell/env.bash
@@ -6,5 +6,6 @@ export PIP_REQUIRE_VIRTUALENV=false
 export PIP_RESPECT_VIRTUALENV=true
 export PIP_DOWNLOAD_CACHE=$HOME/.pip_download_cache
 export TMUXIFIER_LAYOUT_PATH=$HOME/.dotfiles/tmuxifier-layouts
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 
 export PATH=/usr/local/share/npm/bin:~/bin:~/.dotfiles/tmuxifier/bin:$PATH
