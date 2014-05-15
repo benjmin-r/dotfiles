9a6b890d12c37b24a1046cfc6a85e74960bb642d
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-05-15 17:42:48 +0200

    making bash includes more resilient

--- shell/homebrew-completion.bash
+++ shell/homebrew-completion.bash
@@ -1,4 +1,6 @@
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+if [ hash brew 2> /dev/null ]; then
+    if [ -f $(brew --prefix)/etc/bash_completion ]; then
+      . $(brew --prefix)/etc/bash_completion
+    fi
 fi
 
--- shell/ssh-completion.bash
+++ shell/ssh-completion.bash
@@ -1,1 +1,1 @@
-complete -o default -o nospace -W "$(grep -i -e '^host ' ~/.ssh/config | awk '{print substr($0, index($0,$2))}' ORS=' ')" ssh scp sftp
+[ -f ~/.ssh/config ] && complete -o default -o nospace -W "$(grep -i -e '^host ' ~/.ssh/config | awk '{print substr($0, index($0,$2))}' ORS=' ')" ssh scp sftp
