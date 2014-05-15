b210e399f63acf1b52e3d79077e2fea9fef3d8d9
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-05-15 17:31:02 +0200

    gpg-agent should be quiet when it's not present

--- shell/gpg.bash
+++ shell/gpg.bash
@@ -2,5 +2,6 @@ if [ -f "${HOME}/.gpg-agent-info" ]; then
     . "${HOME}/.gpg-agent-info"
     export GPG_AGENT_INFO
 else
-    eval $(gpg-agent --daemon --write-env-file "${HOME}/.gpg-agent-info")
+    [ hash gpg-agent 2> /dev/null ] && \
+        eval $(gpg-agent --daemon --write-env-file "${HOME}/.gpg-agent-info")
 fi
