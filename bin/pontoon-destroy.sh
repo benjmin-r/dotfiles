8c1eec25c62c574c77d3d168405fffa9d8f27e0e
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-05-25 21:54:42 +0200

    pimping my pontoon wrapper scripts with more usefulness

--- bin/pontoon-destroy.sh
+++ bin/pontoon-destroy.sh
@@ -6,9 +6,9 @@ SNAPSHOTNAME="$DROPLET-`date +%Y%m%d`"
 workon pontoon
 
 pontoon droplet shutdown $DROPLET
-sleep 5
+echo "sleeping for 30s" && sleep 30
 pontoon droplet poweroff $DROPLET
-sleep 5
+echo "sleeping for 15s" && sleep 15
 pontoon droplet snapshot $DROPLET $SNAPSHOTNAME
-sleep 10
+echo "sleeping for 5min" && sleep 300
 pontoon droplet destroy $DROPLET
