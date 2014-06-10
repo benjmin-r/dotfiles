d1f91a32410b9c49f9f1f9e8a8c8f7fdb59798d6
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-06-10 15:44:03 +0200

    optimizing pontoon-create script to be more resilient

--- bin/pontoon-destroy.sh
+++ bin/pontoon-destroy.sh
@@ -5,8 +5,11 @@ SNAPSHOTNAME="$DROPLET-`date +%Y%m%d`"
 
 pontoon droplet shutdown $DROPLET
 echo "sleeping for 30s" && sleep 30
-pontoon droplet poweroff $DROPLET
+pontoon droplet poweroff $DROPLET --yes
 echo "sleeping for 15s" && sleep 15
 pontoon droplet snapshot $DROPLET $SNAPSHOTNAME
-echo "sleeping for 5min" && sleep 300
+echo "sleeping for 6min" && sleep 360
 pontoon droplet destroy $DROPLET
+echo "sleeping for 1min" && sleep 60
+echo "\nDestroy finished! Check that '$DROPLET' is not active anymore!\n"
+pontoon droplet list
