c2b8a82d9d5b1d09653a4f23cca8aa2876310bf3
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-07-14 13:37:01 +0200

    making snapshotting of DO images optional and also logging more output

--- bin/pontoon-destroy.sh
+++ bin/pontoon-destroy.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 DROPLET=${1:-dev}
+SHOULD_SNAPSHOT=${2:-1}
 SNAPSHOTNAME="$DROPLET-`date +%Y%m%d`"
 
 pontoon droplet shutdown $DROPLET
-echo "sleeping for 30s" && sleep 30
+echo "SHUT DOWN ... sleeping for 30s" && sleep 30
 pontoon droplet poweroff $DROPLET --yes
-echo "sleeping for 15s" && sleep 15
-pontoon droplet snapshot $DROPLET $SNAPSHOTNAME
-echo "sleeping for 6min" && sleep 360
+echo "POWERED OFF ... sleeping for 15s" && sleep 15
+if [ "$SHOULD_SNAPSHOT" -eq 1 ]; then
+    pontoon droplet snapshot $DROPLET $SNAPSHOTNAME
+    echo "SNAPSHOTTING ... sleeping for 6min" && sleep 360
+fi
 pontoon droplet destroy $DROPLET
-echo "sleeping for 1min" && sleep 60
+echo "DESTROYING ... sleeping for 1min" && sleep 60
 echo "\nDestroy finished! Check that '$DROPLET' is not active anymore!\n"
 pontoon droplet list
