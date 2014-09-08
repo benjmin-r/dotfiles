c6052a0b8fbfc5bf52c4fe3ebc6aa536259b6cf3
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-09-08 17:00:09 +0200

    extending sleep wait for snapshot operation to 10min

--- bin/pontoon-destroy.sh
+++ bin/pontoon-destroy.sh
@@ -10,7 +10,7 @@ pontoon droplet poweroff $DROPLET --yes
 echo "POWERED OFF ... sleeping for 15s" && sleep 15
 if [ "$SHOULD_SNAPSHOT" -eq 1 ]; then
     pontoon droplet snapshot $DROPLET $SNAPSHOTNAME
-    echo "SNAPSHOTTING ... sleeping for 7min" && sleep 420
+    echo "SNAPSHOTTING ... sleeping for 10min" && sleep 600
 fi
 pontoon droplet destroy $DROPLET
 echo "DESTROYING ... sleeping for 1min" && sleep 60
