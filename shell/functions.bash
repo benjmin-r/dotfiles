3d7aed3e109bac3bf99a5833a5ba6404c3b428c2
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-09-10 13:45:58 +0200

    adding `gittree` function

--- shell/functions.bash
+++ shell/functions.bash
@@ -48,3 +48,37 @@ upgrade() {
 function pless {
   pygmentize $1 | less -r
 }
+
+# The following is ripped off from Chris and the original is at
+# https://github.com/chr4/shellrc/blob/master/rc.d/functions.sh#L31-L60
+# Recursively traverse directory tree for git repositories, run git command
+# e.g.
+#   gittree status
+#   gittree diff
+gittree() {
+  if [ $# -lt 1 ]; then
+    echo "Usage: gittree <command>"
+    return 1
+  fi
+
+  for gitdir in $(find . -type d -name .git); do
+    # Display repository name in blue
+    repo=$(dirname $gitdir)
+    echo -e "\033[34m$repo\033[0m"
+
+    # Run git command in the repositories directory
+    cd $repo && git $@
+    ret=$?
+
+    # Return to calling directory (ignore output)
+    cd - > /dev/null
+
+    # Abort if cd or git command fails
+    if [ $ret -ne 0 ]; then
+      return 1
+    fi
+
+    echo
+  done
+}
+
