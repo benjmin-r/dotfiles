4e1077bb9320a868ce7513c99ad24fc5bf91362f
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-09-17 22:57:05 +0200

    increased timeframe for reporting links

--- bin/git-log-as-md-links.sh
+++ bin/git-log-as-md-links.sh
@@ -2,5 +2,5 @@
 
 git log --reverse \
         --author="`git config user.name`" \
-        --since="1 week ago" \
+        --since="2 week ago" \
         --pretty=format:"[%s](https://github.com/`git config remote.origin.url | sed -e "s/.*:\(.*\).git/\1/g"`/commit/%H)" 
