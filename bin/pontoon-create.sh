893befad3016b92037b43815b78b6c6972d4f09a
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-10-13 23:21:03 +0200

    provide option to connect to droplets with mosh too

--- bin/pontoon-create.sh
+++ bin/pontoon-create.sh
@@ -4,9 +4,11 @@ DROPLET=${1:-dev}
 SNAPSHOT=`pontoon snapshot list | tail -n +2 | sort | tail -n 1 | sed "s/ - //"`
 pontoon droplet create $DROPLET --image=$SNAPSHOT
 
-echo "ssh -A -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking no' " \
-    " -L 9000:127.0.0.1:9000 -L 8000:127.0.0.1:8000 " \
-    "`pontoon droplet field $DROPLET ip_address`" \
-    | pbcopy
+DROPLET_IP_ADDR=`pontoon droplet field $DROPLET ip_address`
+SSH_BASE_ARGS="ssh -A -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking no'"
+SSH_FORWARDS_ARGS="-L 9000:127.0.0.1:9000 -L 8000:127.0.0.1:8000"
+
+echo "$SSH_BASE_ARGS $SSH_FORWARDS_ARGS $DROPLET_IP_ADDR" | pbcopy
+echo "mosh --ssh=\"$SSH_BASE_ARGS\" $DROPLET_IP_ADDR" | pbcopy
 
 echo "Droplet started! IP address is in clipboard"
