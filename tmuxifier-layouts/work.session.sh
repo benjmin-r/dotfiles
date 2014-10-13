bda7e042f61536b3f4c44cc9fc89ddf2ffc72af2
Benjamin Reitzammer <benjamin@ambestengestern.de>
2014-10-13 23:30:21 +0200

    tweaking tmuxifier work layout slightly

--- tmuxifier-layouts/work.session.sh
+++ tmuxifier-layouts/work.session.sh
@@ -4,20 +4,21 @@ if initialize_session "work"; then
     window_root "~/Downloads"
     new_window "mutt-personal"
     run_cmd "mutt-personal"
+    split_h
+    run_cmd "mutt-personal -f ~/.mail/ambestengestern.de/lists/"
 
     window_root "~/Downloads"
     new_window "mutt-work"
     run_cmd "mutt-work"
 
+
     window_root "~/Documents/Vaamo/projects/vaamo-backend"
-    new_window "dev-backend"
+    new_window "vaamo-backend"
     split_h
 
-    window_root "~/Documents/Vaamo/projects/infrastructure"
-    new_window "dev-infrastructure"
-    run_cmd "workon vaamo-infrastructure && clear"
+    window_root "~/Documents/Vaamo/projects"
+    new_window "vaamo-projects"
     split_h
-    run_cmd "workon vaamo-infrastructure && clear"
 
     select_window 1
 fi
