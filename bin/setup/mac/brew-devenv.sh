a1bb5110e93f0598775c26334b63d4456bd6b5b4
Benjamin Reitzammer <benjamin@ambestengestern.de>
2015-01-22 00:50:59 +0100

    finishing up dev env setup scripts

--- bin/setup/mac/brew-devenv.sh
+++ bin/setup/mac/brew-devenv.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
+brew cask install java
+brew cask install postgres # which is postgres.app
 
-pyenv
-pyenv-virtualenv
-python
-postgresql
-protobuf
-qt
-node
-autoconf
-bats
-cmake
-cscope
-fswatch
-gcc46                          l
-gdbm
-gettext
-glib
-gmime
-gmp
-gmp4
-gnutls
-go
-libevent
-libffi
-libgcrypt
-libgpg-error
-libksba
-libmpc08
-libpng
-libtasn1
-libtool
-libyaml
-macvim
-mercurial
+brew install node
+brew install protobuf
+brew install qt
+brew install pyenv
+brew install pyenv-virtualenv
+brew install pyenv-virtualenvwrapper
+
+### install pipsi
+###  curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
+### pipsi install goobook
+
+# autoconf
+# bats
+# cmake
+# cscope
+# fswatch
+# gcc46                          l
+# gdbm
+# gettext
+# glib
+# gmime
+# gmp
+# gmp4
+# gnutls
+# go
+# libevent
+# libffi
+# libgcrypt
+# libgpg-error
+# libksba
+# libmpc08
+# libpng
+# libtasn1
+# libtool
+# libyaml
+# macvim
+# mercurial
 
