eb7b484da72ff1c34f9f926ded3fe930c7875e15
Benjamin Reitzammer <benjamin@ambestengestern.de>
2015-10-28 22:48:45 +0100

    fixing tmuxifier layouts

--- tmuxifier-layouts/vaamode.window.sh
+++ tmuxifier-layouts/vaamode.window.sh
@@ -1,5 +1,6 @@
 window_root "~/Documents/Vaamo/projects/vaamo.de"
 new_window "vaamo.de"
 split_h 50
-run_cmd "workon vaamo-de" 2
+run_cmd "cd ~/Documents/Vaamo/projects/vaamo.de" 1
+run_cmd "cd ~/Documents/Vaamo/projects/vaamo.de && workon vaamo-de" 2
 select_pane 1
--- tmuxifier-layouts/work.session.sh
+++ tmuxifier-layouts/work.session.sh
@@ -5,15 +5,22 @@ if initialize_session "work"; then
     new_window "m personal"
     run_cmd "m personal"
     split_h
-    run_cmd "m personal -f ~/.mail/personal/lists/"
+    #run_cmd "m personal -f ~/.mail/personal/lists/"
+    run_cmd "m personal"
 
     window_root "~/Downloads"
     new_window "m work"
+    run_cmd "m work"
+    split_h
     run_cmd "m work"
 
 
     window_root "~/Documents/Vaamo/projects/vaamo-backend"
     new_window "vaamo-backend"
+    split_h
+
+    window_root "~/data/blog-drafts"
+    new_window "blog-drafts"
     split_h
 
     select_window 1
