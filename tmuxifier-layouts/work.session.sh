e61af222de07b6032af1fcc5ff098b4f4747d9b9
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-03-01 23:44:19 +0100

    adapt std tmux session to new work env

--- tmuxifier-layouts/work.session.sh
+++ tmuxifier-layouts/work.session.sh
@@ -5,16 +5,14 @@ if initialize_session "work"; then
     new_window "m personal"
     run_cmd "m personal"
     split_h
-    run_cmd "m personal"
-
-    window_root "~/Downloads"
-    new_window "m work"
     run_cmd "m work"
-    split_h
-    run_cmd "m work -f ~/.mail/work/2answer"
 
     window_root "~/data/blog-drafts"
     new_window "blog-drafts"
+    split_h
+
+    window_root "~/Documents/crispymtn/projects/keyline"
+    new_window "keyline"
     split_h
 
     select_window 1
