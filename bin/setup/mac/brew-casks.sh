2cacbb0ebb42b210566ff8e7dcd1992b7251630b
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-03-01 22:04:29 +0100

    adapting casks

--- bin/setup/mac/brew-casks.sh
+++ bin/setup/mac/brew-casks.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-brew install caskroom/cask/brew-cask
-
 brew cask install google-chrome
-brew cask install firefox
-brew cask install iterm2
 brew cask install arq
-brew cask install textexpander
 brew cask install bartender
 brew cask install atom
-brew cask install moom
 brew cask install totalfinder
 brew cask install skype
 brew cask install spotify
 brew cask install alfred
-brew cask install dropbox
 brew cask install vlc
 brew cask install moneymoney
+brew cask install things
+brew cask install istat-menus
 
 brew cask install truecrypt
+
+# brew cask install firefox
+# brew cask install iterm2
+# brew cask install moom
+# brew cask install dropbox
--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -6,7 +6,7 @@ import re, subprocess
 def get_keychain_pass(account=None, server=None):
     params = {
         'security': '/usr/bin/security',
-        'command': 'find-internet-password',
+        'command': 'find-generic-password',
         'account': account,
         'server': server,
         'keychain': '~/Library/Keychains/login.keychain',
