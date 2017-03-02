9afdfb3b4db0c31d20e9f6ef8766801a1902a528
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-03-02 10:50:06 +0100

    fixing error msg related to rbenv

--- shell/rvm-rbenv.bash
+++ shell/rvm-rbenv.bash
@@ -1,7 +1,5 @@
 RVM_SCRIPT=~/.rvm/scripts/rvm
 [ -f $RVM_SCRIPT ] && source $RVM_SCRIPT
 
-if hash rbenv 2> /dev/null; then eval "$(rbenv init -)"; fi
-
 export PATH=~/.rbenv/shims:$PATH
-source ~/.rbenv/completions/rbenv.bash
+if hash rbenv 2> /dev/null; then eval "$(rbenv init -)"; fi
