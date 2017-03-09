40d05ffbe74455108e968e978154cd40d3a56c9f
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-03-09 23:51:46 +0100

    finishing zsh migration

--- shell/functions.sh
+++ shell/functions.sh
@@ -6,10 +6,6 @@ function view_markdown() {
     /Applications/Marked.app/Contents/MacOS/Marked $1 &
 }
 
-function ssh_tail() {
-    ssh $1 "tail -f /var/log/upstart/${2}.log"
-}
-
 function pgr() {
     ps -xeo pid,command | grep -i $1 | grep -v grep
 }
@@ -39,29 +35,6 @@ function m() {
 
 function json-prettyprint() {
     cat $1 | python -m json.tool
-}
-
-function upgrade() {
-    local retry=5 count=0
-
-    # retry at most $retry times, waiting 1 minute between each try
-    while true; do
-
-        # Tell apt-get to only download packages for upgrade, and send 
-        # signal 15 (SIGTERM) if it takes more than 10 minutes
-        if timeout -15 600 apt-get --assume-yes --download-only upgrade; then
-            break
-        fi
-        if (( count++ == retry )); then
-            printf 'Upgrade failed\n' >&2
-            return 1
-        fi
-        sleep 60
-    done
-
-    # At this point there should be no more packages to download, so 
-    # install them.
-    apt-get --assume-yes upgrade
 }
 
 function pless {
---
+++ zshrc
@@ -0,0 +1,31 @@
+# Path to your oh-my-zsh installation.
+export ZSH=/Users/benjamin/.dotfiles/oh-my-zsh
+
+ZSH_THEME="benjamin"
+
+HYPHEN_INSENSITIVE="true"
+DISABLE_AUTO_TITLE="true"
+COMPLETION_WAITING_DOTS="true"
+
+# CASE_SENSITIVE="true"
+# DISABLE_AUTO_UPDATE="true"
+# export UPDATE_ZSH_DAYS=13
+# ENABLE_CORRECTION="true"
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+# HIST_STAMPS="mm/dd/yyyy"
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+plugins=(rails git autoenv rbenv vi-mode heroku httpie autojump tig)
+
+source $ZSH/oh-my-zsh.sh
+
+# export LANG=en_US.UTF-8
+export SSH_KEY_PATH="~/.ssh/id_rsa"
+
+source ~/.dotfiles/shell/env.zsh
+source ~/.dotfiles/shell/nvm.zsh
+source ~/.dotfiles/shell/pyenv.sh
+source ~/.dotfiles/shell/rbenv.sh
+source ~/.dotfiles/shell/alias.zsh
+source ~/.dotfiles/shell/functions.sh
+source ~/.dotfiles/shell/tmuxifier.sh
