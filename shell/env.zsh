621af9c13e0088ffe2c9fd8f5316e78f8231a34f
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-08-07 11:31:52 +0200

    set EDITOR to neovim, unset PYTHONPATH to fix pyenv-virtualenvwrapper

--- shell/env.zsh
+++ shell/env.zsh
@@ -1,5 +1,5 @@
 export CLICOLOR=1
-export EDITOR=vi
+export EDITOR=nvim
 export TZ='Europe/Berlin'
 export LESS='-R -F -X'
 
@@ -8,7 +8,8 @@ export PIP_REQUIRE_VIRTUALENV=false
 export PIP_RESPECT_VIRTUALENV=true
 export PIP_DOWNLOAD_CACHE=$HOME/.pip_download_cache
 export TMUXIFIER_LAYOUT_PATH=$HOME/.dotfiles/tmuxifier-layouts
-export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+#export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+unset PYTHONPATH=
 export GOPATH=$HOME/go
 
 export MATES_DIR=~/.contacts/personal
