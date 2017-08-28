fd42a79b02bbfef77f0c01b5b488bef698c9a41a
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-08-28 15:36:19 +0200

    switch fish theme to oh-my-fish/theme-cmorrell.com

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,43 +1,86 @@
-# based on https://github.com/oh-my-fish/theme-cbjohnson/blob/master/fish_prompt.fish
-function fish_prompt
-  # Cache exit status
-  set -l last_status $status
+# based on https://github.com/oh-my-fish/theme-cmorrell.com
 
-  # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set -q __fish_prompt_char
-    switch (id -u)
-      case 0
-        set -g __fish_prompt_char \u276f\u276f
-      case '*'
-        set -g __fish_prompt_char »
+
+set -g pad " "
+
+## Function to show a segment
+function prompt_segment -d "Function to show a segment"
+  # Get colors
+  set -l bg $argv[1]
+  set -l fg $argv[2]
+
+  # Set 'em
+  set_color -b $bg
+  set_color $fg
+
+  # Print text
+  if [ -n "$argv[3]" ]
+    echo -n -s $argv[3]
+  end
+end
+
+## Function to show current status
+function show_status -d "Function to show the current status"
+  if [ $RETVAL -ne 0 ]
+    prompt_segment red white " ▲ "
+    set pad ""
     end
+  if [ -n "$SSH_CLIENT" ]
+      prompt_segment blue white " SSH: "
+      set pad ""
+    end
+end
+
+function show_virtualenv -d "Show active python virtual environments"
+  if set -q VIRTUAL_ENV
+    set -l venvname (basename "$VIRTUAL_ENV")
+    prompt_segment normal white " ($venvname)"
+  end
+end
+
+## Show user if not default
+function show_user -d "Show user"
+  if [ "$USER" != "$default_user" -o -n "$SSH_CLIENT" ]
+    set -l host (hostname -s)
+    set -l who (whoami)
+    #prompt_segment normal yellow " $who"
+    prompt_segment normal yellow "$who"
+
+    ## Skip @ bit if hostname == username
+    #if [ "$USER" != "$HOST" ]
+    #  prompt_segment normal white "@"
+    #  prompt_segment normal green "$host "
+    #  set pad ""
+    #end
+  end
+end
+
+# Show directory
+function show_pwd -d "Show the current directory"
+  set -l pwd (prompt_pwd)
+  prompt_segment normal blue "$pad$pwd"
+end
+
+# Show prompt w/ privilege cue
+function show_prompt -d "Shows prompt with cue for current priv"
+  set -l uid (id -u $USER)
+  if [ $uid -eq 0 ]
+    prompt_segment red white " ! "
+    set_color normal
+    echo -n -s " "
+  else
+    prompt_segment normal white " \$ "
   end
 
-  # Setup colors
-  set -l normal (set_color normal)
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l bpurple (set_color -o purple)
-  set -l bred (set_color -o red)
-  set -l bcyan (set_color -o cyan)
-  set -l bwhite (set_color -o white)
+  set_color normal
+end
 
-  # Configure __fish_git_prompt
-  set -g __fish_git_prompt_show_informative_status true
-  set -g __fish_git_prompt_showcolorhints true
-
-  # Color prompt char red for non-zero exit status
-  set -l pcolor $bpurple
-  if [ $last_status -ne 0 ]
-    set pcolor $bred
-  end
-
-  # Top
-  echo -n $cyan$USER$normal in $bred(prompt_pwd)$normal
-  __fish_git_prompt
-
-  echo
-
-  # Bottom
-  echo -n $pcolor$__fish_prompt_char $normal
+## SHOW PROMPT
+function fish_prompt
+  set -g RETVAL $status
+  show_status
+  show_virtualenv
+  show_user
+  show_pwd
+  show_prompt
 end
---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,30 @@
+# based on https://github.com/oh-my-fish/theme-cmorrell.com
+
+function get_git_status -d "Gets the current git status"
+  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
+    set -l ref (command git describe --tags --exact-match ^/dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+
+    if [ "$dirty" != "0" ]
+      set_color -b normal
+      set_color yellow
+      echo "$dirty changed file"
+      if [ "$dirty" != "1" ]
+        echo "s"
+      end
+      echo " "
+      set_color -b yellow
+      set_color white
+    else
+      set_color -b cyan
+      set_color white
+    end
+
+    echo " $ref "
+    set_color normal
+   end
+end
+
+function fish_right_prompt -d "Prints right prompt"
+  get_git_status
+end
