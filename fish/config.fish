1056ede2ff001dfb66318282294ec4dd9ceacc94
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-09-11 23:05:12 +0200

    reorder config.fish and use `ag` for searching with FZF

--- fish/config.fish
+++ fish/config.fish
@@ -4,27 +4,45 @@ else
   set -gx TERM screen-256color
 end
 
-set -gx EDITOR nvim
 set fish_key_bindings fish_vi_key_bindings
 
 
-source ~/.config/fish/abbr.fish
-source ~/.config/fish/autoenv.fish
-source ~/.config/fish/todotxt.fish
-
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ENV vars
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set -gx EDITOR nvim
 set -gx PATH $PATH ~/bin
 set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/.dotfiles/tmuxifier/bin
 
+set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# source in various configs
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+source ~/.config/fish/abbr.fish
+source ~/.config/fish/autoenv.fish
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# tmuxifier && tmux
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set -gx TMUXIFIER_LAYOUT_PATH ~/.dotfiles/tmuxifier-layouts
 eval (tmuxifier init - fish)
 
-# setup pyenv shims ... virtualenv is used as a pyenv plugin
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# nvm & node
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set -gx NVM_DIR ~/.nvm
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# setup pyenv shims ... virtualenv is used as a pyenv plugin
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ... and in case installing pipsi fails
 # ~/.pyenv/shims/pip install -U pip pipsi
-
-set -gx NVM_DIR ~/.nvm
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
