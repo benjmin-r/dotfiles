4d08252b2de81c9f84c0d6cdccc4fd582a955f28
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-10-02 13:49:22 +0200

    deleting lots of unused stuff

--- scm_breeze/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# uninstall by (github: bernardofire)
-# Remove line from bashrc and zshrc if present.
-for rc in bashrc zshrc; do
-  sed -i '/scm_breeze/d' "$HOME/.$rc"
-  printf "Removed SCM Breeze from %s\n" "$HOME/.$rc"
-done
