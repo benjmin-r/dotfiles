fe97e9f863753e939710c108e3e6705ba2216619
Benjamin Reitzammer <benjamin@ambestengestern.de>
2018-04-10 15:35:15 +0200

    separate tmux sessions into work and personal
    ... and update tmux status bar

---
+++ tmuxifier-layouts/personal.session.sh
@@ -0,0 +1,28 @@
+if initialize_session "personal"; then
+    window_root "~/Downloads"
+    new_window "personal"
+    run_cmd "m personal"
+    split_h
+    run_cmd "cd ~/data/notes"
+    select_pane 1
+
+    window_root "~/data/projects/git-projects/squeakyvessel.com"
+    new_window "[sq]"
+    run_cmd "source .env.fish"
+    split_h 70
+    run_cmd "source .env.fish"
+    select_pane 1
+
+
+    window_root "~/data/projects/git-projects/cto.coffee"
+    new_window "[cto.coffee]"
+    run_cmd "source .env.fish"
+    split_h 70
+    run_cmd "source .env.fish"
+    select_pane 1
+
+
+    select_window 1
+fi
+
+finalize_and_go_to_session
--- tmuxifier-layouts/work.session.sh
+++ tmuxifier-layouts/work.session.sh
@@ -1,11 +1,5 @@
 if initialize_session "work"; then
     window_root "~/Downloads"
-    new_window "personal"
-    run_cmd "m personal"
-    split_h
-    run_cmd "cd ~/data/notes"
-
-    window_root "~/Downloads"
     new_window "work"
     run_cmd "m work"
     split_h 60
@@ -13,9 +7,12 @@ if initialize_session "work"; then
 
     window_root "~/work/crispymtn/projects/keyline"
     new_window "keyline"
+    run_cmd "source .env.fish"
     split_h 60
+    run_cmd "source .env.fish"
 
     select_window 1
+    select_pane 1
 fi
 
 finalize_and_go_to_session
