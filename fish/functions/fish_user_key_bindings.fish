86f8c70d2d6c3d6a7cb2ea3c137789e748b212d2
Benjamin Reitzammer <benjamin@ambestengestern.de>
2018-08-14 14:44:29 +0200

    change fish keybinding ... I pressed ctrl-e too often by accident

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -16,14 +16,14 @@ function fish_user_key_bindings
   set -q FZF_LEGACY_KEYBINDINGS
     or set -l FZF_LEGACY_KEYBINDINGS 1
   if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
-    bind \ce 'nvim (fzf)'
+    bind \cp 'nvim (fzf)'
     bind \ct '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
     bind \ex '__fzf_find_and_execute'
     bind \eo '__fzf_cd'
     bind \eO '__fzf_cd_with_hidden'
     if bind -M insert >/dev/null ^/dev/null
-        bind -M insert \ce 'nvim (fzf)'
+        bind -M insert \cp 'nvim (fzf)'
         bind -M insert \ct '__fzf_find_file'
         bind -M insert \cr '__fzf_reverse_isearch'
         bind -M insert \ex '__fzf_find_and_execute'
