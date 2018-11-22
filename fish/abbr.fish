3df5bc25cac9f918fb5954f1c191407db7d62399
Benjamin Reitzammer <benjamin@ambestengestern.de>
2018-11-22 23:28:06 +0100

    add code maat helpers

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -7,3 +7,6 @@ abbr -a tx tmuxifier
 abbr -a sync-contacts "PYTHONPATH='' vdirsyncer sync and mates index"
 
 abbr -a se source .env.fish
+
+# code maat specific abbreviation
+abbr -a gm "git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --before=(date +%Y-%m-%d)"
---
+++ fish/functions/maat.fish
@@ -0,0 +1,4 @@
+function maat -d "Run code-maat"
+    java -jar ~/data/projects/git-projects/code-maat/target/code-maat-1.1-SNAPSHOT-standalone.jar -c git $argv
+end
+
