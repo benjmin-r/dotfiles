178402391e7d5cb8223edbdff17fc1578ee63af2
Benjamin Reitzammer <benjamin@ambestengestern.de>
2024-04-14 13:38:56 +0200

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -4,22 +4,14 @@ else
   set -gx TERM screen-256color
 end
 
-set fish_key_bindings fish_vi_key_bindings
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ENV vars
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-set -gx CARGO_BIN_PATH ~/.cargo/bin
-
 set -gx EDITOR vim
 set -gx SHELL '/opt/homebrew/bin/fish'
 set -gx PATH $PATH ~/bin
-set -gx PATH $PATH $CARGO_BIN_PATH
 set -gx PATH $PATH ~/.tmuxifier/bin
-
-set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
-set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -29,34 +21,24 @@ source ~/.config/fish/abbr.fish
 source ~/.profile
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Make brew completions work
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+eval (/opt/homebrew/bin/brew shellenv)
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # tmuxifier && tmux
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set -gx TMUXIFIER_LAYOUT_PATH ~/.dotfiles/tmuxifier-layouts
 eval (tmuxifier init - fish)
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# nvm & node
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-set -gx NVM_DIR ~/.nvm
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# chruby & gem_home - a better way to do rbenv & bundler
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-source ~/.config/fish/gem_home.fish
-
-# configure brew installed chruby
-source /usr/local/Cellar/chruby-fish/1.0.0/share/fish/vendor_conf.d/chruby_auto.fish
-
-# install ruby versions with `ruby-build 2.5.1 ~/.rubies/2.5.1`
-# `env RUBY_CONFIGURE_OPTS=--with-readline-dir=(brew --prefix readline) ruby-build 2.5.3 ~/.rubies/2.5.3`
-# init default ruby version and cross-project gems
-### commented out, because it makes fish startup considerably slow 
-### and in effect also switching tmux panes from within vim
-#chruby 2.5.1
-#gem_home ~/.gem_home
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -67,5 +49,70 @@ source /usr/local/Cellar/chruby-fish/1.0.0/share/fish/vendor_conf.d/chruby_auto.
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
-# make crystal lang work
-#set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# key bindings
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+fish_vi_key_bindings
+
+bind --mode insert --sets-mode default jk backward-char force-repaint
+
+bind -M visual -m insert c kill-selection end-selection force-repaint
+
+bind \cl 'clear; commandline -f repaint'
+bind -M insert \cl 'clear; commandline -f repaint'
+
+bind -M insert \cf forward-word
+
+bind \cu backward-kill-line
+
+bind -M default V edit_command
+bind -M default K commandline_man
+
+bind --mode insert --sets-mode default jk backward-char force-repaint
+
+bind -M visual -m insert c kill-selection end-selection force-repaint
+
+bind \cl 'clear; commandline -f repaint'
+bind -M insert \cl 'clear; commandline -f repaint'
+
+bind -M insert \cf forward-word
+
+bind \cu backward-kill-line
+
+bind -M default V edit_command
+bind -M default K commandline_man
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ENV vars
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#set -gx CARGO_BIN_PATH ~/.cargo/bin
+#set -gx PATH $PATH $CARGO_BIN_PATH
+
+#set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
+#set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# nvm & node
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#set -gx NVM_DIR ~/.nvm
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# chruby & gem_home - a better way to do rbenv & bundler
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#source ~/.config/fish/gem_home.fish
+
+# configure brew installed chruby
+#source /usr/local/Cellar/chruby-fish/1.0.0/share/fish/vendor_conf.d/chruby_auto.fish
+
+# install ruby versions with `ruby-build 2.5.1 ~/.rubies/2.5.1`
+# `env RUBY_CONFIGURE_OPTS=--with-readline-dir=(brew --prefix readline) ruby-build 2.5.3 ~/.rubies/2.5.3`
+# init default ruby version and cross-project gems
+### commented out, because it makes fish startup considerably slow 
+### and in effect also switching tmux panes from within vim
+#chruby 2.5.1
+#gem_home ~/.gem_home
+
+
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -13,38 +13,36 @@ function fish_user_key_bindings
   bind -M default V edit_command
   bind -M default K commandline_man
 
-  set -q FZF_LEGACY_KEYBINDINGS
-    or set -l FZF_LEGACY_KEYBINDINGS 1
-  if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
-    bind \cp 'nvim (fzf)'
-    bind \ct '__fzf_find_file'
-    bind \cr '__fzf_reverse_isearch'
-    bind \ex '__fzf_find_and_execute'
-    bind \eo '__fzf_cd'
-    bind \eO '__fzf_cd_with_hidden'
-    if bind -M insert >/dev/null ^/dev/null
-        bind -M insert \cp 'nvim (fzf)'
-        bind -M insert \ct '__fzf_find_file'
-        bind -M insert \cr '__fzf_reverse_isearch'
-        bind -M insert \ex '__fzf_find_and_execute'
-        bind -M insert \eo '__fzf_cd'
-        bind -M insert \eO '__fzf_cd_with_hidden'
-    end
-  else
-    bind \cf '__fzf_find_file'
-    bind \cr '__fzf_reverse_isearch'
-    bind \ex '__fzf_find_and_execute'
-    bind \eo '__fzf_cd'
-    bind \eO '__fzf_cd_with_hidden'
-    if bind -M insert >/dev/null ^/dev/null
-        bind -M insert \cf '__fzf_find_file'
-        bind -M insert \cr '__fzf_reverse_isearch'
-        bind -M insert \ex '__fzf_find_and_execute'
-        bind -M insert \eo '__fzf_cd'
-        bind -M insert \eO '__fzf_cd_with_hidden'
-    end
-  end
+  #set -q FZF_LEGACY_KEYBINDINGS
+  #  or set -l FZF_LEGACY_KEYBINDINGS 1
+  #if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
+  #  bind \cp 'nvim (fzf)'
+  #  bind \ct '__fzf_find_file'
+  #  bind \cr '__fzf_reverse_isearch'
+  #  bind \ex '__fzf_find_and_execute'
+  #  bind \eo '__fzf_cd'
+  #  bind \eO '__fzf_cd_with_hidden'
+  #  if bind -M insert >/dev/null ^/dev/null
+  #      bind -M insert \cp 'nvim (fzf)'
+  #      bind -M insert \ct '__fzf_find_file'
+  #      bind -M insert \cr '__fzf_reverse_isearch'
+  #      bind -M insert \ex '__fzf_find_and_execute'
+  #      bind -M insert \eo '__fzf_cd'
+  #      bind -M insert \eO '__fzf_cd_with_hidden'
+  #  end
+  #else
+  #  bind \cf '__fzf_find_file'
+  #  bind \cr '__fzf_reverse_isearch'
+  #  bind \ex '__fzf_find_and_execute'
+  #  bind \eo '__fzf_cd'
+  #  bind \eO '__fzf_cd_with_hidden'
+  #  if bind -M insert >/dev/null ^/dev/null
+  #      bind -M insert \cf '__fzf_find_file'
+  #      bind -M insert \cr '__fzf_reverse_isearch'
+  #      bind -M insert \ex '__fzf_find_and_execute'
+  #      bind -M insert \eo '__fzf_cd'
+  #      bind -M insert \eO '__fzf_cd_with_hidden'
+  #  end
+  #end
   ### fzf ###
 end
-
-fzf_key_bindings
