2703485f6c331d1aee5d343e39b986805c9c22a7
Benjamin Reitzammer <benjamin@ambestengestern.de>
2024-05-06 12:47:04 +0200

    removing obsolte fish functions

--- fish/functions/m.fish
+++
@@ -1,6 +0,0 @@
-function m -d "start mutt"
-    set accountname $argv[1]
-    set --erase argv[1]
-    cd ~/Downloads
-    mutt -n -e "source ~/.mutt/accounts/$accountname" $argv
-end
--- fish/functions/maat.fish
+++
@@ -1,4 +0,0 @@
-function maat -d "Run code-maat"
-    java -jar ~/data/projects/git-projects/code-maat/target/code-maat-1.1-SNAPSHOT-standalone.jar -c git $argv
-end
-
